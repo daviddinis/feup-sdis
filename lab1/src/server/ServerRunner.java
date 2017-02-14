0a21111ce2420358de1715fccd04286274a4be1e
NunoRamos <nunor07.unfc@gmail.com>
2017-02-14 18:23:40 +0000

    Forgot adding the file

---
+++ lab1/src/server/ServerRunner.java
@@ -0,0 +1,22 @@
+package server;
+
+import java.io.IOException;
+
+/**
+ * Created by Nuno on 14/02/2017.
+ */
+public class ServerRunner {
+    private static Server server;
+
+    public static void main(String[] args) throws IOException {
+        if(args.length != 1){
+            throw new IllegalArgumentException("\nBad usage: java server.ServerRunner portNumber");
+        }
+
+        int portNumber = Integer.parseInt(args[0]);
+
+        server = new Server(portNumber);
+
+        server.running();
+    }
+}
