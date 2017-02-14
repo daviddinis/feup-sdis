ac19d2d0182561773c4a04ecbd9716d0663fef87
epassos26 <epassos26@gmail.com>
2017-02-14 12:02:42 +0000

    Added Initial files for lab1

---
+++ lab1/src/client/Client.java
@@ -0,0 +1,14 @@
+package client;
+
+/**
+ * Created by epassos on 2/14/17.
+ */
+public class Client {
+    public static void main(String[] args){
+        /**
+         * send_request();
+         * recv_resp();
+         * process_resp();
+         */
+    }
+}
---
+++ lab1/src/server/Server.java
@@ -0,0 +1,14 @@
+package server;
+
+/**
+ * Created by epassos on 2/14/17.
+ */
+public class Server {
+    public static void main(){
+        /**
+         * recv_request();
+         * process_req();
+         * send_resp();
+         */
+    }
+}
