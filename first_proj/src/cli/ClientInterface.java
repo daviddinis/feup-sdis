388ab2baf5836d3106c522de07570d1e4e0a2bf1
Nuno Ramos <nunor07.unfc@gmail.com>
2017-03-09 22:15:05 +0000

    Starting with rmi client

---
+++ first_proj/src/cli/ClientInterface.java
@@ -0,0 +1,15 @@
+package src.cli;
+
+import src.peers.PeerService;
+
+import java.rmi.Naming;
+
+/**
+ * Created by nuno on 09-03-2017.
+ */
+public class ClientInterface {
+
+    public static void main(String args[]){
+       // PeerService obj = (PeerService) Naming.lookup("")
+    }
+}
--- first_proj/src/peers/PeerService.java
+++ first_proj/src/peers/PeerService.java
@@ -1,12 +1,20 @@
 package src.peers;
 
+import src.common.InitiatorInterface;
+
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
-public class PeerService {
+public class PeerService extends UnicastRemoteObject implements InitiatorInterface{
 
     private String serverId;
+    private String protocolVersion;
+    private String accessPoint;
 
     private MulticastSocket mcSocket;
     private InetAddress mcAddr;
@@ -20,10 +28,12 @@ public class PeerService {
     private InetAddress mdrAddr;
     private int mdrPort;
 
-    public PeerService(String serverId,InetAddress mcAddr,int mcPort,InetAddress mdbAddr,int mdbPort,
+    public PeerService(String serverId,String protocolVersion, String accessPoint,InetAddress mcAddr,int mcPort,InetAddress mdbAddr,int mdbPort,
                        InetAddress mdrAddr,int mdrPort) throws IOException {
 
         this.serverId = serverId;
+        this.protocolVersion = protocolVersion;
+        this.accessPoint = accessPoint;
 
         this.mcAddr = mcAddr;
         this.mcPort = mcPort;
@@ -38,15 +48,46 @@ public class PeerService {
         System.out.println("Multicast data backup addr: "+ this.mdbAddr+" port: "+ this.mdbPort);
         System.out.println("Multicast data restore addr: "+ this.mdrAddr+" port: "+ this.mdrPort);
 
-        mcSocket = new MulticastSocket(this.mcPort);
+      /*  mcSocket = new MulticastSocket(this.mcPort);
         mcSocket.joinGroup(this.mcAddr);
 
         mdbSocket = new MulticastSocket(this.mdbPort);
         mdbSocket.joinGroup(this.mdbAddr);
 
         mdrSocket = new MulticastSocket(this.mdrPort);
-        mdrSocket.joinGroup(this.mdrAddr);
+        mdrSocket.joinGroup(this.mdrAddr);*/
+      
+     /*   try{
+            Naming.rebind("peerObj",this);
+        }catch (Exception e){
+            System.out.println("Peer error: "+ e.getMessage());
+            e.printStackTrace();
+        }*/
 
     }
 
+    @Override
+    public void backup(String pathname, int replicationDegree) throws RemoteException {
+        System.out.println("Dar tudo no backup");
+    }
+
+    @Override
+    public void restore(String pathname) throws RemoteException {
+
+    }
+
+    @Override
+    public void delete(String pathname) throws RemoteException {
+
+    }
+
+    @Override
+    public void reclaim(int maxDiskSpace) throws RemoteException {
+
+    }
+
+    @Override
+    public void state() throws RemoteException {
+
+    }
 }
