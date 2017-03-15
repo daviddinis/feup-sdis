80f4a8a578f0f1f5f015f17a38549c28c04eeebe
Nuno Ramos <nunor07.unfc@gmail.com>
2017-03-15 15:54:54 +0000

    Merging again....

--- proj1/src/peers/PeerClientLink.java
+++ proj1/src/peers/PeerClientLink.java
@@ -2,12 +2,22 @@ package peers;
 
 import common.InitiatorInterface;
 
+import javax.xml.bind.DatatypeConverter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class PeerClientLink extends UnicastRemoteObject implements InitiatorInterface {
 
@@ -63,5 +73,34 @@ public class PeerClientLink extends UnicastRemoteObject implements InitiatorInte
     @Override
     public void state() throws RemoteException {
 
+    }
+
+    private String getFileHash(String pathname) throws IOException {
+        Path path = Paths.get(pathname);
+
+        BasicFileAttributes basicAttr = Files.readAttributes(path, BasicFileAttributes.class);
+
+        //Getting format date
+        FileTime creationTime = basicAttr.creationTime();
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        String dateCreated = df.format(creationTime.toMillis());
+
+        String stringToHash = pathname + dateCreated;
+
+        //Making hash
+        MessageDigest hashDigest;
+        byte[] hash = null;
+
+        try {
+            hashDigest = MessageDigest.getInstance("SHA-256");
+
+            hashDigest.update(stringToHash.getBytes());
+
+            hash = hashDigest.digest();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return DatatypeConverter.printHexBinary(hash);
     }
 }
