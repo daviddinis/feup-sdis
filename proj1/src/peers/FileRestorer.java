eb3244f0a2e8182268bd08f8b1f1be0e61ba43a4
epassos26 <epassos26@gmail.com>
2017-04-04 10:06:24 +0100

    Adding file restorer

---
+++ proj1/src/peers/FileRestorer.java
@@ -0,0 +1,72 @@
+package peers;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class FileRestorer {
+
+    /**
+     * Stores chunks to be written to the file
+     * key = chunk number
+     * value = byte array
+     */
+    private ConcurrentHashMap<String, byte[]> restoredChunks;
+
+    private int nChunks;
+    private String filepath;
+    private String restoredFilesPath;
+    private PeerService peer;
+    private String fileID;
+
+    public FileRestorer(PeerService peer, String filepath, String restoredFilesPath, String fileID) {
+        this.peer = peer;
+        this.filepath = filepath;
+        this.restoredFilesPath = restoredFilesPath;
+        this.fileID = fileID;
+        nChunks = -1;
+        restoredChunks = new ConcurrentHashMap<>();
+    }
+
+    public void processRestoredChunks(String chunkNo, byte[] chunkData) {
+        if (!restoredChunks.containsKey(chunkNo)) {
+            restoredChunks.put(chunkNo, chunkData);
+
+            /* Is this the last chunk? */
+            if (chunkData.length != PeerService.CHUNK_SIZE) {
+                nChunks = Integer.parseInt(chunkNo) + 1;
+            }
+
+            /* Are all the chunks restored? */
+            if (restoredChunks.size() == nChunks) {
+                try {
+                    restoreFile();
+                    peer.markRestored(fileID);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * Write the complete file when the chunks are restored
+     *
+     * @throws FileNotFoundException
+     */
+    private void restoreFile() throws FileNotFoundException {
+        FileOutputStream chunkFile = new FileOutputStream(restoredFilesPath + "/" + filepath, true);
+
+        for (int i = 0; i < restoredChunks.size(); i++) {
+            try {
+                chunkFile.write(restoredChunks.get(Integer.toString(i)));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.format("File %s restored", filepath);
+    }
+
+}
