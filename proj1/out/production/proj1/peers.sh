3d6ccb19eda6464237827f9d31b2c466ecc4c1f7
Nuno Ramos <nunor07.unfc@gmail.com>
2017-04-09 17:18:33 +0100

    Restore enhancement done again, now with tcp connections

--- proj1/out/production/proj1/peers.sh
+++ proj1/out/production/proj1/peers.sh
@@ -7,10 +7,10 @@ xterm -e "rmiregistry " 2> /dev/null &
 sleep 1
 
 #Peers
-xterm -e "java peers.PeerLauncher 1 1.1 peer1 224.0.0.0 4445 224.0.0.1  4446 224.0.0.2 2000 " 2> /dev/null &  
-xterm -e "java peers.PeerLauncher 2 1.1 peer2 224.0.0.0 4445 224.0.0.1  4446 224.0.0.2 2000 " 2> /dev/null & 
-xterm -e "java peers.PeerLauncher 3 1.1 peer3 224.0.0.0 4445 224.0.0.1  4446 224.0.0.2 2000 " 2> /dev/null & 
-xterm -e "java peers.PeerLauncher 4 1.1 peer4 224.0.0.0 4445 224.0.0.1  4446 224.0.0.2 2000 " 2> /dev/null &  
+xterm -e "java peers.PeerLauncher 1 1.2 peer1 224.0.0.0 4445 224.0.0.1  4446 224.0.0.2 2000 " 2> /dev/null &  
+xterm -e "java peers.PeerLauncher 2 1.2 peer2 224.0.0.0 4445 224.0.0.1  4446 224.0.0.2 2000 " 2> /dev/null & 
+xterm -e "java peers.PeerLauncher 3 1.2 peer3 224.0.0.0 4445 224.0.0.1  4446 224.0.0.2 2000 " 2> /dev/null & 
+xterm -e "java peers.PeerLauncher 4 1.2 peer4 224.0.0.0 4445 224.0.0.1  4446 224.0.0.2 2000 " 2> /dev/null &  
 
 echo "Launched peers..." 
 wait
